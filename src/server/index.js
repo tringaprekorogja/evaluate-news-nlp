f92fe973496f6a43210d3d6c8211309a7d0e4359
tringa-prekorogja <t.prekorogja@gmail.com>
2020-03-12 22:30:13 +0100

    Add environment variables

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 var aylien = require("aylien_textapi");
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+    });
 
 
 const app = express()
