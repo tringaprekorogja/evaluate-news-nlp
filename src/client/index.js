89b2c80c90273734366c2c0092bcf4b5db4daedc
tringa-prekorogja <t.prekorogja@gmail.com>
2020-03-12 15:57:20 +0100

    Add project setup

--- src/client/index.js
+++ src/client/index.js
@@ -5,3 +5,14 @@ console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+export {
+    checkForName,
+    handleSubmit 
+}
