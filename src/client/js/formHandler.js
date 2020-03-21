1f39ad61b488b507f09a9c5876892c885e3856b7
tringa-prekorogja <t.prekorogja@gmail.com>
2020-03-21 12:09:58 +0100

    Add test for getSentiment function

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 function handleSubmit(event) {
     event.preventDefault()
 
--- src/client/js/formHandler.test.js
+++ src/client/js/formHandler.test.js
@@ -5,7 +5,7 @@ import {getSentiment} from './formHandler';
 test('response from the request contains the data in form of an object', () => {
        return getSentiment('https://www.theguardian.com/music/2020/mar/19/david-bowie-50-greatest-songs-ranked')
         .then(result => {
-            expect(result).toBeDfined()
-            expect(result).polarity.toEqual('positive');
+            expect(result).toBeDefined()
+            expect(result.polarity).toEqual('positive');
         });
 });
